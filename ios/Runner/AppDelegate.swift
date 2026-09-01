5c6318d08a688fc9aca72df31d4da81eb37f10a1
yohesh <info.vistaratech@gmail.com>
2026-09-01 14:32:43 +0530

    Initialize WidgetsFlutterBinding and provide GoogleMaps Navigation API Key in AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleNavigation
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBC-9h-ad-8t_40wZCkUeB1TPhQUvfGelM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:cleadr/src/app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const CleadrApp());
-
-/// ----- TODOs -----
-/// 1. Code refactoring
-///    - const
-///    - GoogleMapsNavigator
-///    - inconsistent widget formatting
-/// 2. _onMapClicked - Snaps to closest place pin
-/// 3. After exiting navigation, map still shows the route preview *
-/// 4. Left landscape <-> Right landscape
-/// 4. Finalising design
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const CleadrApp());
+}
